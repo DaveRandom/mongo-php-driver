212ce0872e9448dbe5521d7e143f371dbe2865e4
Joe Watkins <krakjoe@php.net>
2015-12-13 07:07:38 +0000

    fix is_public_property

--- src/bson.c
+++ src/bson.c
@@ -1051,7 +1051,10 @@ static bool is_public_property(zend_class_entry *ce, const char *prop_name, int 
 	ZVAL_STRINGL(&member, prop_name, prop_name_len);
 	property_info = zend_get_property_info(ce, Z_STR(member), 1 TSRMLS_CC);
 	zval_ptr_dtor(&member);
-	return (property_info && property_info != ZEND_WRONG_PROPERTY_INFO && (property_info->flags & ZEND_ACC_PUBLIC));
+	if (!property_info || property_info == ZEND_WRONG_PROPERTY_INFO || property_info->flags & ZEND_ACC_PUBLIC) {
+		return true;
+	}
+	return false;
 #else
 	ZVAL_STRINGL(&member, prop_name, prop_name_len, 0);
 	property_info = zend_get_property_info(ce, &member, 1 TSRMLS_CC);
